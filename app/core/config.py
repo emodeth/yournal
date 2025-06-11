a6d07e59ebf803e31b061dc08ffad1a404b7eaa4
CemCanbolat <cemcanbolat159@gmail.com>
2025-06-11 19:29:49 +0300

    Prepare render deployment with Docker and env config

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,9 @@
 import os
 from datetime import timedelta
 from dotenv import load_dotenv
-from redis import Redis
+from redis import from_url as redis_from_url
 
 load_dotenv()
-
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev')
@@ -12,15 +11,14 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
 
-    # Configure session management
+    # Session configuration
     SESSION_TYPE = 'redis'
-    SESSION_REDIS = Redis(host=os.getenv('REDIS_HOST', 'localhost'),port=int(os.getenv('REDIS_PORT', 6379)))
+    SESSION_REDIS = redis_from_url(os.getenv('REDIS_URL', 'redis://localhost:6379'))
     SESSION_PERMANENT = False
-    SESSION_USE_SIGNER = False 
+    SESSION_USE_SIGNER = os.getenv('SESSION_USE_SIGNER', 'False').lower() == 'true'
     SESSION_KEY_PREFIX = 'session:'
     SESSION_COOKIE_NAME = 'my_session'
-    # SESSION_FILE_DIR = './api/sessions'
-    SESSION_COOKIE_SAMESITE = 'None'
-    SESSION_COOKIE_SECURE = True
- 
+    SESSION_COOKIE_SAMESITE = os.getenv('SESSION_COOKIE_SAMESITE', 'Lax')
+    SESSION_COOKIE_SECURE = os.getenv('SESSION_COOKIE_SECURE', 'False').lower() == 'true'
+
     PERMANENT_SESSION_LIFETIME = timedelta(days=int(os.getenv('PERMANENT_SESSION_LIFETIME', 3)))
